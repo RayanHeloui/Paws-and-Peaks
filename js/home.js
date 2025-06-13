e08aae9858e927b7fdc7bd69970fc28b58f1c0b3
PaP <woof@pawsandpeaks.com.au>
2025-06-13 15:32:25 +1000

    Update home.js

--- js/home.js
+++ js/home.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function () {
       progress = Math.max(0, Math.min(progress, 1));
 
       imgLeft.style.transform = `translate(${lerp(0, 300, progress)}px, ${lerp(170, -60, progress)}px)`;
-      imgCenter.style.transform = `translate(${lerp(-50, -60, progress)}%, ${lerp(-60, 30, progress)}%)`;
+      imgCenter.style.transform = `translate(${lerp(-50, 70, progress)}%, ${lerp(-60, 30, progress)}%)`;
       imgRight.style.transform = `translate(${lerp(0, -100, progress)}px, ${lerp(0, 100, progress)}px)`;
     });
   }
