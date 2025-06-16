8c66ab110b18568eb27a60d45f8dce076f42073e
PaP <woof@pawsandpeaks.com.au>
2025-06-16 12:56:05 +1000

    Update home.js

--- js/home.js
+++ js/home.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
   if (heroSection && imgLeft && imgCenter && imgRight) {
     // Set initial transform based on scroll start
     const initialProgress = 0;
-    imgLeft.style.transform = `translate(${lerp(0, 300, initialProgress)}px, ${lerp(1220, -60, initialProgress)}px)`;
+    imgLeft.style.transform = `translate(${lerp(0, 300, initialProgress)}px, ${lerp(230, -60, initialProgress)}px)`;
     imgCenter.style.transform = `translate(${lerp(-50, 70, initialProgress)}%, ${lerp(-60, 30, initialProgress)}%)`;
     imgRight.style.transform = `translate(${lerp(0, -100, initialProgress)}px, ${lerp(0, 100, initialProgress)}px)`;
 
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function () {
       let progress = (scrollY - start) / (end - start);
       progress = Math.max(0, Math.min(progress, 1));
 
-      imgLeft.style.transform = `translate(${lerp(0, 300, progress)}px, ${lerp(170, -60, progress)}px)`;
+      imgLeft.style.transform = `translate(${lerp(0, 300, progress)}px, ${lerp(230, -60, progress)}px)`;
       imgCenter.style.transform = `translate(${lerp(-50, 70, progress)}%, ${lerp(-60, 30, progress)}%)`;
       imgRight.style.transform = `translate(${lerp(0, -100, progress)}px, ${lerp(0, 100, progress)}px)`;
     });
