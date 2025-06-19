a37da29cae93254366fede9db68644fc2f9cde08
PaP <woof@pawsandpeaks.com.au>
2025-06-19 17:31:00 +1000

    Update home.js

--- js/home.js
+++ js/home.js
@@ -36,7 +36,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     // ✅ Word-by-word animation
-    const thresholds = [1.1, 1.25, 1.4, 1.55];
+   const thresholds = [0.45, 0.55, 0.65, 0.75];
     blocks.forEach((block, index) => {
       if (scrollProgress >= thresholds[index]) {
         block.classList.add("active");
