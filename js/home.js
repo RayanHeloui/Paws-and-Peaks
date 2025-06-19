d493b6469ee7179d6d3dcf9c35701cca0ced097c
PaP <woof@pawsandpeaks.com.au>
2025-06-19 16:32:15 +1000

    Update home.js

--- js/home.js
+++ js/home.js
@@ -30,7 +30,7 @@ if (scrollProgress >= 0.9 && rect.top <= 0 && rect.bottom > 0) {
   sloganSection.classList.remove('visible');
 }
 
-  const thresholds = [0.9, 0.93, 0.96, 0.99];
+  const thresholds = [0.92, 0.94, 0.96, 0.98];
   blocks.forEach((block, index) => {
     if (scrollProgress >= thresholds[index]) {
       block.classList.add('active');
